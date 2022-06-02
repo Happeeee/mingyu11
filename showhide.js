af07a2a345718b5e34d586510f128f8e239a7770
Happeeee <106572156+Happeeee@users.noreply.github.com>
2022-06-02 15:24:38 +0900

    add showhide.js

---
+++ showhide.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    var $menu_id = window.location.hash.substring(1)
+    if ($menu_id == "") {
+      $menu_id = "menu_" + "About_me";
+    }
+    else {
+      $menu_id = "menu_" + $menu_id;
+    }
+    $(".nav-link").each(function () {
+      $(this).removeClass("active");
+      if ($(this).attr("target-menu") == $menu_id) {
+        $(this).addClass("active");
+      }
+
+      $(".menu_template").each(function () {
+        if ($(this).attr("id") == $menu_id) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+
+    });
+
+    $(".nav-link").click(function () {
+      $(".nav-link").removeClass("active");
+      $(this).addClass("active");
+
+      menu_id = $(this).attr("target-menu");
+      $(".menu_template").each(function () {
+        if ($(this).attr("id") == menu_id) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    });
+  });
